ab885ca26f8f5019175a90f541d10a0c968768c9
Abish <abishvijayan7643@gmail.com>
2020-12-13 02:55:22 +0530

    Nodemailer - Production Ready

--- config/server.js
+++ config/server.js
@@ -1,8 +1,8 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: "https://threeiinc-strapi.herokuapp.com",
-  // url: env("URL"),
+  // url: "https://threeiinc-strapi.herokuapp.com",
+  url: env("URL"),
   admin: {
     auth: {
       secret: env("ADMIN_JWT_SECRET", "a4412ca835c04da0c0dd182e392ad78e"),
