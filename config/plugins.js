d44f4ffdfae755f12213a1aff79679e70cdf9e34
Abish <abishvijayan7643@gmail.com>
2020-12-13 02:20:14 +0530

    Added NodeMailer

--- config/plugins.js
+++ config/plugins.js
@@ -1,24 +1,40 @@
 module.exports = ({ env }) => ({
-    // ...
-    upload: {
-      provider: 'cloudinary',
-      providerOptions: {
-        cloud_name: env('CLOUDINARY_NAME'),
-        api_key: env('CLOUDINARY_KEY'),
-        api_secret: env('CLOUDINARY_SECRET'),
-      },
+  // -------------Cloudinary-------------
+  upload: {
+    provider: "cloudinary",
+    providerOptions: {
+      cloud_name: env("CLOUDINARY_NAME"),
+      api_key: env("CLOUDINARY_KEY"),
+      api_secret: env("CLOUDINARY_SECRET"),
     },
-    // ...
-    // ...
-    email: {
-      provider: 'sendgrid',
-      providerOptions: {
-        apiKey: env('SENDGRID_API_KEY'),
-      },
-      settings: {
-        defaultFrom: 'admin@3iinc.xyz',
-        defaultReplyTo: 'admin@3iinc.xyz',
-      },
+  },
+  // ------------------------------------
+  // -------------SendGrid---------------
+  // email: {
+  //   provider: "sendgrid",
+  //   providerOptions: {
+  //     apiKey: env("SENDGRID_API_KEY"),
+  //   },
+  //   settings: {
+  //     defaultFrom: "admin@3iinc.xyz",
+  //     defaultReplyTo: "admin@3iinc.xyz",
+  //   },
+  // },
+  // ------------------------------------
+  // -------------NodeMailer-------------
+  email: {
+    provider: "nodemailer-refactor",
+    providerOptions: {
+      host: env("SMTP_HOST"),
+      port: env("SMTP_PORT"),
+      secure: env("SMTP_SECURE"),
+      username: env("SMTP_USERNAME"),
+      password: env("SMTP_PASSWORD"),
     },
-    // ...
-  });
+    settings: {
+      defaultFrom: "admin@3iinc.xyz",
+      defaultReplyTo: "admin@3iinc.xyz",
+    },
+  },
+  // ------------------------------------
+});
