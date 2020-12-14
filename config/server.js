ca48198602b617fe52593cca932b5464490b67b6
Abish <abishvijayan7643@gmail.com>
2020-12-14 14:31:17 +0530

    Explicitely Adding /admin as dashbaord URL

--- config/server.js
+++ config/server.js
@@ -3,6 +3,7 @@ module.exports = ({ env }) => ({
   port: env.int("PORT", 1337),
   url: env("URL"),
   admin: {
+    url: "/admin",
     auth: {
       secret: env("ADMIN_JWT_SECRET", "a4412ca835c04da0c0dd182e392ad78e"),
     },
