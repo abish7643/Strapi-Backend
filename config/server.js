f9328932d3eca1349af511eff6bc93691aae4458
abish7643 <abishvijayan7643@gmail.com>
2021-05-05 00:03:09 +0530

    Server Error Workaround

--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  auth: {
-    secret: env("ADMIN_JWT_SECRET", "a4412ca835c04da0c0dd182e392ad78e"),
+  admin: {
+    auth: {
+      secret: env("ADMIN_JWT_SECRET", "a4412ca835c04da0c0dd182e392ad78e"),
+    },
   },
 });
