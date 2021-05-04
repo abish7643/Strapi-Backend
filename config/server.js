012c0f7a4ac43c409121b3f2987212f8e3d1e886
abish7643 <abishvijayan7643@gmail.com>
2021-05-04 22:42:00 +0530

    Admin Panel Not Loading - Workaround

--- config/server.js
+++ config/server.js
@@ -1,7 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
-  url: env("URL"),
+  // url: env("URL"),
   admin: {
     url: "/admin",
     auth: {
