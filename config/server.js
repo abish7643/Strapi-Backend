07e33cf51a83e7dc0ece7e3bd3a1e3273c24a686
abish7643 <abishvijayan7643@gmail.com>
2021-05-04 23:34:28 +0530

    Server Error Due to Absense of Admin_JWT_SECRET

--- config/server.js
+++ config/server.js
@@ -1,4 +1,7 @@
 module.exports = ({ env }) => ({
   host: env("HOST", "0.0.0.0"),
   port: env.int("PORT", 1337),
+  auth: {
+    secret: env("ADMIN_JWT_SECRET", "a4412ca835c04da0c0dd182e392ad78e"),
+  },
 });
